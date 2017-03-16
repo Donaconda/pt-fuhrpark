a5d80b127e541de3074c4613a6b2b8c585a96926
Hades2004 <e_bong@web.de>
2017-03-16 13:20:01 +0100

    MainApp + Wrapper Klassen

---
+++ src/model/FahrzeugListWrapper.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "fahrzeuge")
+public class FahrzeugListWrapper {
+
+	private List<Fahrzeug> fahrzeug;
+	
+	 @XmlElement(name = "fahrzeug")
+	 public List<Fahrzeug> getFahrzeug() {
+		 return fahrzeug;
+	 }
+	 
+	 public void setFahrzeug(List<Fahrzeug> fahrzeug) {
+		 this.fahrzeug = fahrzeug;
+	 }
+	
+}
---
+++ src/model/MitarbeiterListWrapper.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "mitarbeiter")
+public class MitarbeiterListWrapper {
+
+	private List<Mitarbeiter> mitarbeiter;
+	
+	 @XmlElement(name = "EinMitarbeiter")
+	 public List<Mitarbeiter> getMitarbeiter() {
+		 return mitarbeiter;
+	 }
+	 
+	 public void setMitarbeiter(List<Mitarbeiter> mitarbeiter) {
+		 this.mitarbeiter = mitarbeiter;
+	 }
+	
+}
