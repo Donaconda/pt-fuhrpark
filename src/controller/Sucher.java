6125cc5d834cf221f025c8b1d186435e7182eb3a
marufan <schwarfe@gmail.com>
2017-03-30 15:14:31 +0200

    doku aktualisiert, Sucher erweitert

--- src/controller/Sucher.java
+++ src/controller/Sucher.java
@@ -8,7 +8,7 @@ import model.Fahrzeug;
 import model.Mitarbeiter;
 
 public class Sucher {
-	
+
 	public static boolean suche(ObservableList<Fahrzeug> liste, Fahrzeug fahrzeug){
 		for(Fahrzeug f : liste){
 			if(f.compareTo(fahrzeug)==0){
@@ -17,7 +17,7 @@ public class Sucher {
 		}
 		return false;
 	}
-	
+
 	public static boolean suche(ObservableList<Mitarbeiter> liste, Mitarbeiter mitarbeiter){
 		for(Mitarbeiter m : liste){
 			if(m.compareTo(mitarbeiter)==0){
@@ -26,7 +26,7 @@ public class Sucher {
 		}
 		return false;
 	}
-	
+
 	public static boolean suche(ObservableList<Buchung> liste, Buchung buchung){
 		for(Buchung b : liste){
 			if(b.compareTo(buchung)==0){
@@ -34,6 +34,24 @@ public class Sucher {
 			}
 		}
 		return false;
+	}
+
+	public static Fahrzeug sucheFahrzeug(ObservableList<Fahrzeug> liste, String fahrzeug){
+		for(Fahrzeug f : liste){
+			if(f.getKennzeichen().compareTo(fahrzeug)==0){
+				return f;
+			}
+		}
+		return null;
+	}
+
+	public static Mitarbeiter sucheMitarbeiter(ObservableList<Mitarbeiter> liste, String mitarbeiter){
+		for(Mitarbeiter m : liste){
+			if(m.toString().compareTo(mitarbeiter) == 0) {
+				return m;
+			}
+		}
+		return null;
 	}
 
 }
