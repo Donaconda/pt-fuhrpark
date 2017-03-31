3ef4b262baa86340dae60864863cee2db4d8f662
koch_m <koch_m@HSL3352G.tiretech.contiwan.com>
2017-03-31 17:39:33 +0200

    Bugfix: Exception beim Laden der Buchungen

--- src/controller/LocalDateTimeAdapter.java
+++ src/controller/LocalDateTimeAdapter.java
@@ -1,19 +1,13 @@
 package controller;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
-    @Override
-    public String marshal(LocalDateTime arg0) throws Exception {
-        return arg0.toString();
+    public LocalDateTime unmarshal(String v) throws Exception {
+        return LocalDateTime.parse(v);
     }
-
-    @Override
-    public LocalDateTime unmarshal(String arg) throws Exception {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
-        return LocalDateTime.parse(arg, formatter);
+    public String marshal(LocalDateTime v) throws Exception {
+        return v.toString();
     }
 }
