4f6231bcd10f8ec80097de702c726a3156e32076
tgma07 <tgma07@GFL7227G.auto.contiwan.com>
2017-05-15 10:46:01 +0200

    Kommentare hinzugefügt

--- src/controller/Sortierer.java
+++ src/controller/Sortierer.java
@@ -4,22 +4,22 @@ import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class Sortierer {
+public class Sortierer {		// Klasse für Selection Sort
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static ObservableList<? extends Comparable> sort(List<? extends Comparable> list) {
 		ObservableList<Object> sortiert = FXCollections.observableArrayList();
-		int listursprungsgröße = list.size();
-		for (int y = 0; y < listursprungsgröße; y++) {
-			Object o = list.get(0);
+		int listursprungsgroeße = list.size();
+		for (int y = 0; y < listursprungsgroeße; y++) { // So viele Durchläufe wie die Ursprungsliste Einträge hat
 			int merker = 0;
-			for (int i = 0; i < list.size(); i++) {
+			Object o = list.get(0);
+			for (int i = 0; i < list.size(); i++) {		// For-Schleife sucht das kleinste verbleibende Element der Liste
 				if (list.get(i).compareTo(o) < 0) {
 					o = list.get(i);
 					merker = i;
 				}
 			}
-			sortiert.add(o);
-			list.remove(merker);
+			sortiert.add(o);							// Kleinstes Element zu sortierter Liste hinzufügen &
+			list.remove(merker);						// aus Ursprungsliste entfernen
 		}
 		return (ObservableList<? extends Comparable>) sortiert;
 	}
